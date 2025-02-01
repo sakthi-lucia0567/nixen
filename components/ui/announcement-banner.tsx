62370a2aa7b36ea0d69c85ef11ee1edec6be3b57
sakthi-cacxi <sakthivel@cacxi.com>
2025-02-01 22:18:12 +0530

    chore: remove errors

--- components/ui/announcement-banner.tsx
+++ components/ui/announcement-banner.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
-const AnnouncementBanner: React.FC<{}> = ({}) => {
+const AnnouncementBanner = () => {
   const [isVisible, setIsVisible] = useState(true);
 
   if (!isVisible) return null;
